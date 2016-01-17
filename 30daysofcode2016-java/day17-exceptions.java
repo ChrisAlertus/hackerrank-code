48a40fd7244413c3601ea0d78cbb321408ceb9db
Chris Alumnus <alertdrive44@gmail.com>
2016-01-17 14:13:41 -0500

    exceptions day 17, note dont make public classes unnecessarily

---
+++ 30daysofcode2016-java/day17-exceptions.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+//Write your code here
+class Calculator {
+    public int power(int n,int p) throws NegativeArgument
+    {
+        if(!(n >0 && p > 0))  throw new NegativeArgument();
+        int ans = 1;
+        for(;p > 0;p--){
+            ans*=n;
+        }
+        //System.out.println(ans);
+        return ans;
+    }      
+}
+
+class NegativeArgument extends Exception
+{
+   public String getMessage()
+   {
+      return "n and p should be non-negative";
+   }
+}
+// end of my code
+class Solution{
+
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+        int T=in.nextInt();
+        while(T-->0)
+        {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try
+            {
+                int ans=myCalculator.power(n,p);
+                System.out.println(ans);
+                
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+}
+
