78d51a0a47488b27bc37811ec3ff5365e9001038
Chris Alumnus <alertdrive44@gmail.com>
2016-01-27 11:33:37 -0500

    useless asst

---
+++ 30daysofcode2016-java/day27-testing.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        // Test CASES
+        System.out.println("5");
+        // YEs
+        System.out.println("10 6");
+        System.out.println("1 0 -1 50 50 50 50 50 50 50"); //0 -1");
+        // No
+        System.out.println("4 1");
+        System.out.println("0 -1000 1000 -500");
+     
+        // Yes
+        System.out.println("6 4");
+        System.out.println("0 -1 -20 300 1000 1000");
+        // No
+        System.out.println("3 2");
+        System.out.println("1000 0 -1");
+        // Yes
+        System.out.println("5 5");
+        System.out.println("1 0 -1 5 10");
+        
+        
+    }
+}
